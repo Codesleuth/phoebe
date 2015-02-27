f438d339cca4efd95550fe723d8c63b9d4565672
David Wood <david.p.wood@gmail.com>
2015-02-27 23:24:13 +0000

    Split logging into request and response logs.

--- bin/phoebe.js
+++ bin/phoebe.js
@@ -2,19 +2,27 @@
 
 var http = require('http'),
     log = require('../lib/logger'),
-    logger = log.middleware(),
+    requestLogger = log.middleware(true),
+    responseLogger = log.middleware(),
     Phoebe = require('../');
 
 var phoebe = Phoebe();
 
 var httpServer = http.createServer(function (req, res) {
-  logger(req, res, function () {
-    phoebe(req, res, function () {
-      res.end();
+  requestLogger(req, res, function () {
+
+    responseLogger(req, res, function () {
+      phoebe(req, res, function () {
+        res.end();
+      });
     });
+
   });
 });
 
-httpServer.listen(8080, function () {
-  console.log("Phoebe's ready on port 8080");
+var port = process.env.port || 80;
+var ip = process.env.IP;
+
+httpServer.listen(port, ip, function () {
+  log.info("Phoebe's ready at http://%s:%d", ip || 'localhost', port);
 });
